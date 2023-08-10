a5a0451af1425f47807f69c07ddd80e938208766
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2023-08-10 12:20:31 +0500

    task2.scala

--- src/main/scala/lab2/task2.scala
+++ src/main/scala/lab2/task2.scala
@@ -4,48 +4,15 @@ import chisel3.util._
 
 class task2 extends Module{
     val io =IO(new Bundle{
-        val in1=Input(Bool())
-        val in2=Input(Bool())
-        val in3=Input(Bool())
-        val in4=Input(Bool())
-
-        val s0=Input(Bool())
-        val s1=Input(Bool())
-
+        val in=Vec(4,Input(Bool()))
+        val s=Vec(2,Input(Bool()))
         val shift=Input(Bool())
-
-        val out0=Output(Bool())
-        val out1=Output(Bool())
-        val out2=Output(Bool())
-        val out3=Output(Bool())
+        val out=Vec(4,Output(Bool()))
     })
-val select=Cat(io.s0,io.s1).asUInt
-io.out0:= MuxLookup(select,false.B,Array(
-    0.U -> io.in1,
-    1.U -> io.in2,
-    2.U -> io.in3,
-    3.U -> io.in4
-)) 
+io.out(0):= Mux(io.s(0),Mux(io.s(1),io.in(3),io.in(2)),Mux(io.s(1),io.in(1),io.in(0)))
+io.out(1):= Mux(io.s(0),Mux(io.s(1),Mux(io.shift,io.in(0),0.U),io.in(2)),Mux(io.s(1),io.in(1),io.in(0)))
+io.out(2):= Mux(io.s(0),Mux(io.s(1),Mux(io.shift,io.in(1),0.U),Mux(io.shift,io.in(0),0.U)),Mux(io.s(1),io.in(1),io.in(0)))
+io.out(3):= Mux(io.s(0),Mux(io.s(1),Mux(io.shift,io.in(2),0.U),Mux(io.shift,io.in(1),0.U)),Mux(io.s(1),Mux(io.shift,io.in(0),0.U),io.in(0)))
 
-io.out1:= MuxLookup(select,false.B,Array(
-    0.U -> io.in1,
-    1.U -> io.in2,
-    2.U -> io.in3,
-    3.U -> Mux(io.shift,io.in1,0.U)
-))
-
-io.out2:= MuxLookup(select,false.B,Array(
-    0.U -> io.in1,
-    1.U -> io.in2,
-    2.U -> Mux(io.shift,io.in1,0.U),
-    3.U -> Mux(io.shift,io.in2,0.U)
-))
-
-io.out3:= MuxLookup(select,false.B,Array(
-    0.U -> io.in1,
-    1.U -> Mux(io.shift,io.in1,0.U),
-    2.U -> Mux(io.shift,io.in2,0.U),
-    3.U -> Mux(io.shift,io.in3,0.U)
-))
 
 }

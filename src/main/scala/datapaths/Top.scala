1f6a2484338966160e06d5bbce1df7ac545deb3d
AdeiaSaif <120249675+AdeiaSaif@users.noreply.github.com>
2023-09-15 15:51:54 +0500

    Update Top.scala

--- src/main/scala/datapaths/Top.scala
+++ src/main/scala/datapaths/Top.scala
@@ -24,15 +24,11 @@ class Top extends Module{
     dontTouch(load.io)
     val immgen=Module(new imm)
     dontTouch(immgen.io)
-    val jlr= Module(new Jalr)
-    dontTouch(jlr.io)
+    val jalr=Module(new Jalr)
+    dontTouch(jalr.io)
 
-  
-     // Jalr
-    jlr.io.a:=reg.io.d1
-    jlr.io.b:=immgen.io.out
-   
-
+    jalr.io.a:=(pc.io.out).asSInt
+    jalr.io.b:=immgen.io.out
     //imm generator
     immgen.io.ins:=instmem.io.dout
     //inst mem
@@ -53,14 +49,13 @@ class Top extends Module{
          /*load*/  Mux(cu.io.ins(6,0)==="b0000011".U, load.io.din,
                    Mux(cu.io.ins(6,0)==="b0110111".U,immgen.io.out,
                    Mux(cu.io.ins(6,0)==="b0010111".U,(pc.io.out+cu.io.auipc).asSInt,
-                   Mux(cu.io.ins(6,0)==="b1101111".U,((pc.io.out+4.U).asSInt),
-                   Mux(cu.io.ins(6,0)==="b1100011".U,alu.io.alu_out,
-                   Mux(cu.io.ins(6,0)==="b1100111".U,((pc.io.out).asSInt)+4.S,0.S)))))))
-     reg.io.wenb:=cu.io.writereg
-    //alu
-    alu.io.arg_x:= reg.io.d1
-    alu.io.arg_y:= Mux((instmem.io.dout(6,0)==="b0110011".U)|| (instmem.io.dout(6,0)==="b0100011".U ) ||  (instmem.io.dout(6,0)==="b1100011".U ),reg.io.d2,
-    Mux((instmem.io.dout(6,0)==="b0010011".U) ||(instmem.io.dout(6,0)==="b0000011".U) ||(instmem.io.dout(6,0)==="b1100111".U),immgen.io.out,0.S))
+                   Mux(cu.io.ins(6,0)==="b1101111".U || cu.io.ins(6,0)==="b1100111".U,((pc.io.out).asSInt+(4.S)),
+                   Mux(cu.io.ins(6,0)==="b1100011".U,alu.io.alu_out,0.S))))))
+                   reg.io.wenb:=cu.io.writereg
+                   //alu
+                   alu.io.arg_x:= reg.io.d1
+                   alu.io.arg_y:= Mux((instmem.io.dout(6,0)==="b0110011".U)|| (instmem.io.dout(6,0)==="b0100011".U ) ||  (instmem.io.dout(6,0)==="b1100011".U ),reg.io.d2,
+                   Mux((instmem.io.dout(6,0)==="b0010011".U) ||(instmem.io.dout(6,0)==="b0000011".U) ||(instmem.io.dout(6,0)==="b1100111".U) ,immgen.io.out,0.S))
     alu.io.alu_op := cu.io.op
     alu.io.opcode:=cu.io.ins(6,0)
 
@@ -77,14 +72,10 @@ class Top extends Module{
      //load
     load.io.fun3:=cu.io.op
     load.io.din:=dmem.io.dout
-    pc.io.enb:=cu.io.enb
+    
+    // pc.io.enb:=cu.io.reg_pc_enb
     pc.io.in:=Mux(instmem.io.dout(6,0)==="b1101111".U,(immgen.io.out).asUInt,//jal
     Mux(instmem.io.dout(6,0)==="b1100011".U,// branch
     Mux(alu.io.alu_out===1.S,(immgen.io.out<<1.U).asUInt,4.U),
-    Mux(instmem.io.dout(6,0)==="b1100111".U,  (jlr.io.out).asUInt,4.U)))
-
-
-   // pc.io.op:=instmem.io.dout(6,0)
-
-
+    Mux(instmem.io.dout(6,0)==="b1100111".U, ((jalr.io.out).asUInt),4.U)))//jalr
 }
